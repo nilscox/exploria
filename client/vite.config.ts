3227c739947e5c0381b24fc204aa8369b8fb2d3d
nils <nilscox.dev@gmail.com>
2026-07-09 11:23:46 +0200

    add cache control header to service worker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,42 @@ import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import type { ManifestOptions } from 'vite-plugin-pwa';
 import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import manifest from './manifest.json';
+const manifest: Partial<ManifestOptions> = {
+  name: 'Exploria',
+  short_name: 'Exploria',
+  theme_color: '#e17100',
+  display: 'standalone',
+  start_url: '/session',
+  icons: [
+    {
+      src: 'pwa-64x64.png',
+      sizes: '64x64',
+      type: 'image/png',
+    },
+    {
+      src: 'pwa-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: 'pwa-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any',
+    },
+    {
+      src: 'maskable-icon-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+  ],
+};
 
 export default defineConfig({
   plugins: [
